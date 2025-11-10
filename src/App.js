40855ae240910bf2e6f00d11677d5f6fe5f16000
matheusjtavares <67846971+matheusjtavares@users.noreply.github.com>
2025-11-10 20:47:47 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>GameShare</h1>
+      <h2>Play Games</h2>
+      <h2>Share experiences.</h2>
+    </div>
+  );
+}
